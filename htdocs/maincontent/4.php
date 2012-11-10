ef6b688ca1ac450197b98968693edaaefd4920e7
wengshiy <wengshiy@gmail.com>
2012-11-10 23:27:33 +0200

    some modifications

--- htdocs/maincontent/4.php
+++ htdocs/maincontent/4.php
@@ -1,11 +1,11 @@
 <div class="prices" style="margin:20px 35px 0;">
-	<h1>Web Maintenance</h1>
+	<h1 style="text-align: center;">Web Maintenance</h1>
 	<div class="wide">
 		<p>Keep your website up-to-date!</p>
 		<!--<p><span class="mini">495€ + CPO provision + VAT</span></p>-->
 		<h3><a class="price" id="order_starter" href="<?php echo $_SERVER['REQUEST_URI']; ?>#page_orderForm">Ask for an offer!</a></h3>
 	</div>
-	<h1>Web Design and Development</h1>
+	<h1 style="text-align: center;">Web Design and Development</h1>
 	<div class="left">
 		<p>There is a special offer for a basic website with 0 euro! Only costs you website maintanence fee monthly! </p>
 		<!--<p><span class="mini">895€ + CPO provision + VAT</span></p>-->
@@ -14,12 +14,12 @@
 	<div class="right">
 		<img src="images/prices_new.jpg" alt="" style="padding:5px;" />
 	</div>
-	<h1>SNS Apps</h1>
+	<h1 style="text-align: center;">SNS Apps</h1>
 	<div class="wide">
-		<p>A Facebook Webstore? Sounds cool? Let's design yours!</p>
+		<p>A Facebook direct marketing apps? Sounds cool? Get yours immediately!</p>
 		<h3><a class="price" id="order_custom" href="<?php echo $_SERVER['REQUEST_URI']; ?>#page_orderForm">Ask for an offer!</a></h3>
 	</div>
-	<h1>Advertising</h1>
+	<h1 style="text-align: center;">Advertising</h1>
 	<div class="left">
 		<p>Tailored advertising campaign is waiting for you!</p>
 		<p>Custom made design according to your brand.&nbsp;</p>
