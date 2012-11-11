e420903a0c21b723f41ed1f4f5847e0a5084946f
wengshiy <wengshiy@gmail.com>
2012-11-11 02:35:04 +0200

    contact page image added

--- htdocs/maincontent/7.php
+++ htdocs/maincontent/7.php
@@ -1,13 +1,13 @@
 <div style="padding:20px 35px;">
 	<h1 class="center" style="text-align: center;">Contact</h1>
-
+        <img class="lazy" data-original="images/contact.jpg" src="img/image.gif" alt="Contact us" style="float: right; margin:30px 50px 0 0;"/>
 	<div>
 		<h2>Project Director</h2>
 		<p>Shiying Weng</p>
 		<p>shiying.weng@innolake.com</p>
 		<p><?php echo 'Tel: ' . $fphone1; ?></p>		  
 	</div>	
-    <div class="row-fluid" style="margin-top: 20px;">
+        <div style="margin-top: 20px;">
 		<h2>Marketing Director</h2>
 		<p>Shirley You</p>
 		<p>shirley.you@innolake.com</p>
