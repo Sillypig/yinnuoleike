853a3bb65fa4850dd1101d555c9f0889c130f928
wengshiy <wengshiy@gmail.com>
2013-02-23 12:05:52 +0200

    problems solved

--- htdocs/Vip-j/wp-config.php
+++ htdocs/Vip-j/wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'vip-j');
+define('DB_NAME', 'innolake_vip-j');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'innolake_root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'root10312012');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'box873.bluehost.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
